6a29efae5efb8b4ba30113b46723a8023b7675b8
lolcapm <amayberc@gmail.com>
2018-08-16 19:24:11 -0400

    Practica de shooter remasterizado

---
+++ Practica Programacion grafica 2/Assets/Scripts/OverPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OverPlayer : MonoBehaviour {
+
+    public float maxSalud;
+    public float curSalud;
+    public GameObject ui;
+
+    // AudioSource playerAudio;
+
+
+    void Awake()
+    {
+        //playerAudio = GetComponent<AudioSource>();
+
+        curSalud = maxSalud;
+    }
+    void Update()
+    {
+        if (curSalud <= 0)
+        {
+            //playerAudio.clip = deathClip;
+            //playerAudio.Play();
+            SceneManager.LoadScene("Perdiste");
+        }
+    }
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("ObjetoADestruir1"))
+        {
+            curSalud -= 10;
+        }
+    }
+}
---
+++ Practica Programacion grafica 2/Assets/Scripts/SistemaLost.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SistemaLost : MonoBehaviour {
+
+	public void reinicio()
+    {
+        SceneManager.LoadScene("Nivel 1");
+    }
+    public void salida()
+    {
+        Application.Quit();
+    }
+}
